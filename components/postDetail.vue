565574ca1dca72271d0a9ef8c7628708530fd4f1
h-hiroki <gyouzakuuka@gmail.com>
2019-10-02 21:37:09 +0900

    create component

---
+++ components/postDetail.vue
@@ -0,0 +1,45 @@
+<template>
+<v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title class="align-end fill-height">{{ post.title }}</v-card-title>
+    </v-img>
+
+    <v-card-text>
+      <span>{{ post.publishedAt }}</span><br>
+      <span class="text--primary">
+        <span>hugahuga</span><br>
+        <span>Whitsunday Island, Whitsunday Islands</span>
+      </span>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        text
+        color="orange"
+      >
+        Share
+      </v-btn>
+      <v-btn
+        text
+        color="orange"
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'post'
+  ]
+}
+</script>
